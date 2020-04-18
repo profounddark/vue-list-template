5ed3eab6c181f9fd285ea958444ce92bc944b1b6
Andrew Asplund <aasplund@Apollo.local>
2020-04-17 20:41:01 -0700

    oops i forgot

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,184 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="pokedex" id="pokedex">
+    <!-- div for the pokedex entry and v-for -->
+    <div v-for="pokemon in pokedex" :key="pokemon.name">
+      <h2>{{ pokemon.name }}</h2>
+      <!-- Image of the Pokemon -->
+      <img v-bind:src="imageSrc + pokemon.index + '.png'">
+      <h3>National Index #{{ pokemon.index }}</h3>
+      <p>{{ pokemon.description }}</p>
+      <ul class="stats">
+        <li><strong>Height:</strong> {{ pokemon.height | inchToFeet }}</li>
+        <li><strong>Weight:</strong> {{ pokemon.weight }} lbs</li>
+      </ul>
+      <h3 v-if="pokemon.types.length <= 1">Type:</h3>
+      <h3 v-else>Types:</h3>
+      <ul class="poketype">
+        <li v-for="type in pokemon.types" v-bind:class="type" :key="type">{{ type }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import data from pokedata.js file
+import pokedata from "./assets/pokedata.js";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data: function() {
+    return pokedata;
+  },
+  filters: {
+    inchToFeet: function(number) {
+      let feet = Math.floor(number / 12);
+      let inches = number % 12;
+      return `${feet}' ${inches}"`;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  font-family: sans-serif;
+  background-color: black;
+}
+ul {
+  list-style-type: none;
+  padding: 0px;
+}
+
+.pokedex {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+}
+
+.pokedex img {
+  width: 100%;
+}
+
+.pokedex > div {
+  padding: 1rem;
+  border: solid;
+  border-width: 3px;
+  border-color: black;
+  background-color: white;
+}
+
+h2,
+h3 {
+  font-family: sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-transform: uppercase;
+}
+
+.stats {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+}
+
+.stats > li {
+  padding: 0.1rem 0.5rem;
+}
+
+.poketype {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+}
+.poketype li {
+  display: inline;
+  padding: 0.1rem 0.5rem;
+  margin: 0 0.5rem;
+}
+.poketype li {
+  font-family: sans-serif;
+  color: white;
+  background-color: gray;
+  text-transform: uppercase;
+}
+
+.poketype .bug {
+  background-color: #a8b820;
+}
+
+.poketype .dark {
+  background-color: #705848;
+}
+
+.poketype .dragon {
+  background-color: #7038f8;
+}
+
+.poketype .electric {
+  background-color: #f8d030;
+}
+
+.poketype .fairy {
+  background-color: #ee99ac;
+}
+
+.poketype .fighting {
+  background-color: #c03028;
+}
+
+.poketype .fire {
+  background-color: #f08030;
+}
+
+.poketype .flying {
+  background-color: #a890f0;
+}
+
+.poketype .ghost {
+  background-color: #705898;
+}
+
+.poketype .grass {
+  background-color: #78c850;
+}
+
+.poketype .ground {
+  background-color: #e0c068;
+}
+
+.poketype .ice {
+  background-color: #98d8d8;
+}
+
+.poketype .normal {
+  background-color: #a8a878;
+}
+
+.poketype .poison {
+  background-color: #a040a0;
+}
+
+.poketype .psychic {
+  background-color: #f85888;
+}
+
+.poketype .rock {
+  background-color: #b8a038;
+}
+
+.poketype .steel {
+  background-color: #b8b8d0;
+}
+
+.poketype .water {
+  background-color: #6890f0;
+}
+
+@media screen and (min-width: 800px) {
+  .pokedex {
+    margin: 0 auto;
+  }
+  .pokedex > div {
+    max-width: 400px;
+  }
 }
 </style>
---
+++ src/assets/pokedata.js
@@ -0,0 +1,101 @@
+export default {
+    imageSrc: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/",
+    pokedex: [
+        {
+            name: "Bulbasaur",
+            index: "001",
+            description: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.",
+            height: 28,
+            weight: 15.2,
+            types: ["grass"]
+        },
+        {
+            name: "Ivysaur",
+            index: "002",
+            description: "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.",
+            height: 39,
+            weight: 28.7,
+            types: ["grass", "poison"]
+        },
+        {
+            name: "Venusaur",
+            index: "003",
+            description: "There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people.",
+            height: 79,
+            weight: 220.5,
+            types: ["grass", "poison"]
+        },
+        {
+            name: "Charmander",
+            index: "004",
+            description: "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.",
+            height: 24,
+            weight: 18.7,
+            types: ["fire"]
+        },
+        {
+            name: "Charmeleon",
+            index: "005",
+            description: "Charmeleon mercilessly destroys its foes using its sharp claws. If it encounters a strong foe, it turns aggressive. In this excited state, the flame at the tip of its tail flares with a bluish white color.",
+            height: 43,
+            weight: 41.9,
+            types: ["fire"]
+        },
+        {
+            name: "Charizard",
+            index: "006",
+            description: "Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.",
+            height: 67,
+            weight: 199.5,
+            types: ["fire", "flying"]
+        },
+        {
+            name: "Squirtle",
+            index: "007",
+            description: "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds.",
+            height: 20,
+            weight: 19.8,
+            types: ["water"]
+        },
+        {
+            name: "Wartortle",
+            index: "008",
+            description: "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler.",
+            height: 39,
+            weight: 49.6,
+            types: ["water"]
+        },
+        {
+            name: "Blastoise",
+            index: "009",
+            description: "Blastoise has water spouts that protrude from its shell. The water spouts are very accurate. They can shoot bullets of water with enough accuracy to strike empty cans from a distance of over 160 feet.",
+            height: 63,
+            weight: 188.5,
+            types: ["water"]
+        },
+        {
+            name: "Caterpie",
+            index: "010",
+            description: "Caterpie has a voracious appetite. It can devour leaves bigger than its body right before your eyes. From its antenna, this Pokémon releases a terrifically strong odor.",
+            height: 12,
+            weight: 6.4,
+            types: ["bug"]
+        },
+        {
+            name: "Metapod",
+            index: "011",
+            description: "The shell covering this Pokémon's body is as hard as an iron slab. Metapod does not move very much. It stays still because it is preparing its soft innards for evolution inside the hard shell.",
+            height: 28,
+            weight: 21.8,
+            types: ["bug"]
+        },
+        {
+            name: "Butterfree",
+            index: "012",
+            description: "Butterfree has a superior ability to search for delicious honey from flowers. It can even search out, extract, and carry honey from flowers that are blooming over six miles from its nest.",
+            height: 43,
+            weight: 70.5,
+            types: ["bug", "flying"]
+        }
+    ]
+}
